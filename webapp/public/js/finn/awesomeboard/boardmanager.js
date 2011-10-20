c501b84e598e35b2492ba6b92fd757e522acde6e
leftie friele <leftie.friele@gmail.com>
2011-10-20 22:02:06 +0200

    added the roatation...

--- webapp/public/js/finn/awesomeboard/boardmanager.js
+++ webapp/public/js/finn/awesomeboard/boardmanager.js
@@ -5,17 +5,26 @@ FINN.awesomeboard.boardmanager = (function(eventHub, mu){
 	var templateId = "",
 		tweets = [],
 		currentIndex = 0,
-		maxIndex = 0;
-	function updateBoard(){
+		maxIndex = 0,
+		TWEET_WAIT_TIME = 6000;
+	function updateBoard() {
 		if (maxIndex==currentIndex) currentIndex = 0;
 		var focusTweet = mu.to_html(document.getElementById(templateId).innerHTML, tweets[currentIndex]);
 		$(".awesomeboard:first").html(focusTweet);
+	}
+	function rotateTweets(){
+		currentIndex = currentIndex + 1;
+		updateBoard();
+		setTimeout(rotateTweets, TWEET_WAIT_TIME);		
 	}
 	function initialize(options){
 		eventHub.subscribe("got-new-tweets", function(newTweets){
 			tweets = newTweets;
 			maxIndex = tweets.length;
 			currentIndex = 0;
+		});
+		eventHub.subscribe("got-new-tweets", function(){
+			setTimeout(rotateTweets, TWEET_WAIT_TIME);					
 		});
 		eventHub.subscribe("templates-ready", function(template){
 			templateId = template;
